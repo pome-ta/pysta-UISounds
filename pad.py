a0ed9aa742fcba2f33c2fcdad8cb65d2f3b8fe47
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-08 12:23:31 +0900

    up

--- pad.py
+++ pad.py
@@ -12,7 +12,8 @@ root_path = root  # / new
 
 
 class Pad(ui.View):
-  def __init__(self):
+  def __init__(self, *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'slategray'
     self.note = None
 
@@ -26,7 +27,8 @@ class Pad(ui.View):
 
 
 class WrapGrid(ui.View):
-  def __init__(self, g_size):
+  def __init__(self, g_size, *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
     self.height = g_size
     self.width = g_size
     self.border_width = 1
@@ -43,8 +45,12 @@ class WrapGrid(ui.View):
 
 
 class RackGrid(ui.View):
-  def __init__(self):
+  def __init__(self, *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'steelblue'
+    self.scroll_view = ui.ScrollView()
+    self.scroll_view.flex = 'WH'
+    #self.add_subview(self.scroll_view)
     self.set_pads()
 
   @ui.in_background
@@ -55,7 +61,7 @@ class RackGrid(ui.View):
     set_size = min(_w, _h) * 0.25
     count = 0
     pad_line = 4
-    for n, x in enumerate(range(32)):
+    for n, x in enumerate(range(64)):
       wrap = WrapGrid(set_size)
       set_y = int(-(-n / pad_line))
       if n % 4 == 0:
@@ -68,17 +74,20 @@ class RackGrid(ui.View):
       count += 1
       wrap.pad.note = sound.Player(str(sound_list[n]))
       wrap.pad.add_subview(num)
+      #self.scroll_view.add_subview(wrap)
       self.add_subview(wrap)
 
 
 class MainView(ui.View):
-  def __init__(self):
+  def __init__(self, *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'red'
     self.rack = RackGrid()
     self.rack.flex = 'WH'
     self.add_subview(self.rack)
 
 
-view = MainView()
-view.present('fullscreen')
+if __name__ == '__main__':
+  view = MainView()
+  view.present('fullscreen')
 
